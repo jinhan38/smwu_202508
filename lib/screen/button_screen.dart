f43e8227104ca34a4634b247bccc73cfd2cfcecd
jinhankim <jinhan38@naver.com>
2025-08-20 14:30:12 +0900

    column_row_practice_screen.dart 추가

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -3,6 +3,7 @@ import 'package:smwu_202508/screen/column/column_practice_screen.dart';
 import 'package:smwu_202508/screen/column/column_screen.dart';
 import 'package:smwu_202508/screen/container/container_practice_screen.dart';
 import 'package:smwu_202508/screen/container/container_screen.dart';
+import 'package:smwu_202508/screen/row/column_row_practice_screen.dart';
 import 'package:smwu_202508/screen/row/row_practice_screen.dart';
 import 'package:smwu_202508/screen/row/row_screen.dart';
 
@@ -98,6 +99,20 @@ class ButtonScreen extends StatelessWidget {
                 );
               },
               child: Text("Row 실습"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ColumnRowPracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Column, Row 심화"),
             ),
           ],
         ),
