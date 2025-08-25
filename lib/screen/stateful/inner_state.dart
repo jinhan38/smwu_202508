cbae764efa3ef9309380c31dee8fd7d5b4f908b8
jinhan <jinhan38@naver.com>
2025-08-25 14:23:49 +0900

    inner_state.dart 추가

--- lib/screen/stateful/inner_state.dart
+++ lib/screen/stateful/inner_state.dart
@@ -10,8 +10,28 @@ class InnerState extends StatefulWidget {
 }
 
 class _InnerStateState extends State<InnerState> {
+  int innerCount = 0;
+
+  /// initState 는 Stateful 위젯이 생성될 때 한 번만 호출됨.
+  @override
+  void initState() {
+    super.initState();
+    innerCount = widget.count;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(widget.count.toString());
+    return Column(
+      children: [
+        Text(innerCount.toString()),
+        ElevatedButton(
+          onPressed: () {
+            innerCount++;
+            setState(() {});
+          },
+          child: Text("Inner count up"),
+        ),
+      ],
+    );
   }
 }
