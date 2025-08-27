351a02bb5e42c53cbe9581634751fe6a1ccf9804
jinhan <jinhan38@naver.com>
2025-08-27 16:12:13 +0900

    회원 정보 수정

--- lib/screen/network/member_edit_screen.dart
+++ lib/screen/network/member_edit_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class MemberEditScreen extends StatefulWidget {
@@ -10,8 +11,10 @@ class MemberEditScreen extends StatefulWidget {
 }
 
 class _MemberEditScreenState extends State<MemberEditScreen> {
-
+  Dio dio = Dio(BaseOptions(baseUrl: "https://0f5d227aa566.ngrok-free.app"));
   TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
 
   @override
   void initState() {
@@ -19,6 +22,13 @@ class _MemberEditScreenState extends State<MemberEditScreen> {
     emailController.text = widget.email;
   }
 
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +36,18 @@ class _MemberEditScreenState extends State<MemberEditScreen> {
     body: Column(
       children: [
         TextFormField(controller: emailController),
+        TextFormField(controller: passwordController),
+        TextFormField(controller: descriptionController),
+        ElevatedButton(onPressed: () async{
+          await dio.patch('/api/v1/member/update', data: {
+            "email" : emailController.text,
+            "password" : passwordController.text,
+            'description' : descriptionController.text,
+          });
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("수정 성공")));
+          await Future.delayed(Duration(milliseconds: 800));
+          Navigator.pop(context);
+        }, child: Text("업데이트")),
       ],
     ),);
   }
