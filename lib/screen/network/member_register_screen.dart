d6d9ccc9be19616e190e65e27b51321ceec14a2b
jinhan <jinhan38@naver.com>
2025-08-27 14:46:00 +0900

    member_register_screen.dart UI 추가

--- lib/screen/network/member_register_screen.dart
+++ lib/screen/network/member_register_screen.dart
@@ -8,10 +8,29 @@ class MemberRegisterScreen extends StatefulWidget {
 }
 
 class _MemberRegisterScreenState extends State<MemberRegisterScreen> {
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("멤버 등록"),
-    ),
+    return Scaffold(appBar: AppBar(title: Text("멤버 등록"),),
+      body: Column(
+        children: [
+          TextFormField(controller: emailController),
+          TextFormField(controller: passwordController),
+          ElevatedButton(onPressed: () {
+
+          }, child: Text("등록")),
+        ],
+      ),
     );
   }
 }
