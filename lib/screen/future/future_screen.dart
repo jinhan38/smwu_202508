fec5712bf6d37dc56fb5fc6d3eb760e1b17a1c76
jinhan <jinhan38@naver.com>
2025-08-27 14:15:40 +0900

    future_screen.dart 작업중

--- lib/screen/future/future_screen.dart
+++ lib/screen/future/future_screen.dart
@@ -10,15 +10,23 @@ class FutureScreen extends StatefulWidget {
 class _FutureScreenState extends State<FutureScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Future")),
-    body: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        ElevatedButton(onPressed: () {
-
-        }, child: Text("Delay"))
-
-      ],
-    ),);
+    return Scaffold(
+      appBar: AppBar(title: Text("Future")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              print("시작 전");
+              await Future.delayed(Duration(milliseconds: 1000), () {
+                print('Delay 끝');
+              });
+              print("시작 후");
+            },
+            child: Text("Delay"),
+          ),
+        ],
+      ),
+    );
   }
 }
